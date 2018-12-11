5d9350f8362a026ca28c04e83d284008841fd3a8
wahyugunawan <44379935+wahyugunawan@users.noreply.github.com>
2018-12-12 00:58:43 +0700

    Lampiran untuk soal 6.3b modus dan nilai max
    Menentukan nilai modus pada Array dan nilai maksinalnya

--- main.cpp
+++ main.cpp
@@ -1,44 +1,92 @@
 #include <iostream>
 
+
 using namespace std;
 
-int main () {
-int a = 0;
-int i;
-int sum = 0, max = 0, min = 0;
+int main()
+{
+   int max,x,n,k=1;
+   float bil[10];
+   float mod[10];
+   int total[10];
 
+   x=0;
+   cout<<"Banyak Data yang ingin di input : ";cin>>n;
+   for(int c=0;c<n;c++)
+   {
+            cout<<"Data Ke- "<<(c+1)<<" : ";cin>>bil[c];
+   }
+   cout<<endl;
 
-cout << "Banyaknya element yang akan diinput :";
-cin >> a;
-cout <<endl;
+   for(int c=0;c<n;c++)
+   {
+    for(int i=(n-1);i>=0;i--)
+    {
+    if(bil[i]<bil[i-1])
+    {
+    int temp;
+    temp=bil[i];
+    bil[i]=bil[i-1];
+    bil[i-1]=temp;
+    }
+    }
+    }
 
-int data[a];
+    for(int c=0;c<n;c++)
+    {total[c]=0;
+    for(int i=0;i<n;i++)
+    {
+    if(bil[c]==bil[i])
+    {
+    total[c]++;
+    }}}
 
+    for(int c=0;c<n;c++)
+    {
+    if(total[c]>k)
+    {k=total[c];}
+    }
 
-for(i=0; i<a; i++) {
-cout << "Input element ke ";
-cout << i+1;
-cout << " : ";
-cin >> data[i];
-}
+    for(int c=0;c<n;c++)
+    {
+    if(x==0)
+    mod[x]=0;
+    else
+    mod[x]=mod[x-1];
+    if(total[c]==k)
+    {if(bil[c]!=mod[x])
+    {mod[x]=bil[c];
+    x++;}
+    }
+    }
 
-cout << "\n Tampilan Array :";
-cout << "\n data yang anda masukan adalah :";
-max = min = data[0];
-for(i=0; i<a; i++) {
-cout << data[i] << ',';
+    int z=0;
+    for(int c=0;c<n;c++)
+    {if(total[c]==k){z++;}
+    }
 
-if (max < data[i]) {
-max = data[i];
-}
-if (min > data[i]) {
-min = data[i];
-}
-}
+    if(z==n){x=0;}
 
-cout <<endl;
-cout << "\n Nilai terkecil : " << min;
-cout << "\n Nilai terbesar : " << max;
+    if (x==0)
+    cout<<"Tidak Ada Modus!"<<endl;
+   else
+   {cout<<" Nilai Modus : ";
+    for(int c=0;c<x;c++)
+    {cout<<mod[c]<<" ";}
+    }
+
+    cout<<endl;
+
+    max=0;
+    max = bil[0];
+    for(int c=0;c<n;c++)
+    {
+    if (max < bil[c])
+    {max=bil[c];}
+    }
+    cout<<"\n Nilai terbesar : " << max;
 
 return 0;
 }
+
+
