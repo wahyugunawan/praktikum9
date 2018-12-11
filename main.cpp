78c06af2fd036f86598388fbd80c439a1550d0e3
wahyugunawan <44379935+wahyugunawan@users.noreply.github.com>
2018-12-12 01:02:44 +0700

    lampiran soal 6.5a mengalikan dua buah matrik
    Mengalikan dua buah matrik

--- main.cpp
+++ main.cpp
@@ -1,92 +1,64 @@
 #include <iostream>
 
-
 using namespace std;
 
-int main()
-{
-   int max,x,n,k=1;
-   float bil[10];
-   float mod[10];
-   int total[10];
+int main (){
+int matrik1[2][3];
+int matrik2[3][1];
+int matrik3[2][2];
+int temp;
 
-   x=0;
-   cout<<"Banyak Data yang ingin di input : ";cin>>n;
-   for(int c=0;c<n;c++)
-   {
-            cout<<"Data Ke- "<<(c+1)<<" : ";cin>>bil[c];
-   }
-   cout<<endl;
 
-   for(int c=0;c<n;c++)
-   {
-    for(int i=(n-1);i>=0;i--)
-    {
-    if(bil[i]<bil[i-1])
-    {
-    int temp;
-    temp=bil[i];
-    bil[i]=bil[i-1];
-    bil[i-1]=temp;
-    }
-    }
-    }
+for (int x = 0;x<2;x++){
+for (int y =0;y<3;y++){
+cout <<"masukan nilai matrik A baris ke-"<<(x+1)<<" kolom ke-"<<(y+1)<<" : ";
+cin>>matrik1[x][y];
+}
+}
+cout<<endl;
 
-    for(int c=0;c<n;c++)
-    {total[c]=0;
-    for(int i=0;i<n;i++)
-    {
-    if(bil[c]==bil[i])
-    {
-    total[c]++;
-    }}}
+cout<<"Nilai Matrik A :"<<endl;
+for (int x = 0;x<2;x++){
+for (int y =0;y<3;y++){
+cout <<matrik1[x][y]<<"   ";
+}
+cout<<endl;
+}
+cout<<endl;
 
-    for(int c=0;c<n;c++)
-    {
-    if(total[c]>k)
-    {k=total[c];}
-    }
+for (int x = 0;x<3;x++){
+for (int y =0;y<1;y++){
+cout <<"masukan nilai matrik B baris ke-"<<(x+1)<<" kolom ke-"<<(y+1)<<" : ";
+cin>>matrik2[x][y];
+}
+}
+cout<<endl;
 
-    for(int c=0;c<n;c++)
-    {
-    if(x==0)
-    mod[x]=0;
-    else
-    mod[x]=mod[x-1];
-    if(total[c]==k)
-    {if(bil[c]!=mod[x])
-    {mod[x]=bil[c];
-    x++;}
-    }
-    }
-
-    int z=0;
-    for(int c=0;c<n;c++)
-    {if(total[c]==k){z++;}
-    }
-
-    if(z==n){x=0;}
-
-    if (x==0)
-    cout<<"Tidak Ada Modus!"<<endl;
-   else
-   {cout<<" Nilai Modus : ";
-    for(int c=0;c<x;c++)
-    {cout<<mod[c]<<" ";}
-    }
-
-    cout<<endl;
-
-    max=0;
-    max = bil[0];
-    for(int c=0;c<n;c++)
-    {
-    if (max < bil[c])
-    {max=bil[c];}
-    }
-    cout<<"\n Nilai terbesar : " << max;
-
-return 0;
+cout<<"Nilai Matrik B :"<<endl;
+for (int x = 0;x<3;x++){
+for (int y =0;y<1;y++){
+cout <<matrik2[x][y]<<"   ";
+}
+cout<<endl;
 }
 
+for (int x = 0;x<3;x++){
+for (int y =0;y<1;y++){
+matrik3[x][y]=0;
+for (int z =0;z<3;z++){
+temp=matrik1[x][z]*matrik2[z][y];
+matrik3[x][y]=matrik3[x][y]+temp;
+}
+}
+}
 
+cout<<endl;
+cout<<"Hasil perkalian matrik AxB ="<<endl;
+for (int x = 0;x<2;x++){
+for (int y =0;y<1;y++){
+cout <<matrik3[x][y]<<"   ";
+}
+cout<<endl;
+}
+    return 0;
+}
